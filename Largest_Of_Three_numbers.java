d50320251359c646e75d41cd1c8a04479f63474d
VINOTHPADMANABAN <vinothkumarpadmanabhan@gmail.com>
2025-08-11 23:27:58 +0530

    Largest_Of_Three_numbers

---
+++ Largest_Of_Three_numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Largest_Of_Three_numbers {
+    public static void main(String[] vk){
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        if(n1 > n2 && n1 > n3) System.out.println("The greatest number is " + n1);
+        if(n2 > n1 && n2 > n3) System.out.println("The greatest number is " + n2);
+        if(n3 > n1 && n3 > n1) System.out.println("The greatest number is " + n3);
+        sc.close();
+     }
+}
+
+// This program is for identify the largest number among the three number.
