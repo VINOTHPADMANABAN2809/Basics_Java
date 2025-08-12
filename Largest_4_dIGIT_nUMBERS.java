c04723092eb3c8e4d2052a2c535be9e0295c2908
VINOTHPADMANABAN <vinothkumarpadmanabhan@gmail.com>
2025-08-12 17:36:18 +0530

    Largest Number of four

--- Largest_4_dIGIT_nUMBERS.java
+++ Largest_4_dIGIT_nUMBERS.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Largest_4_dIGIT_nUMBERS {
+public class Largest_4_dIGIT_numbers {
     public static void main(String[] vk){
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
