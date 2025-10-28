7620f05b15f4b897e415915521402e789b55f7ff
VINOTHPADMANABAN2809 <vinothkumarpadmanabhan@example.com>
2025-10-28 11:25:43 +0530

    Special pattern

--- Pattens/outer_box.java
+++ Pattens/outer_box.java
@@ -9,7 +9,7 @@ public class outer_box {
                 if(i==0 || i==n-1 || j==0 || j== n-1){
                     System.out.print("* ");
                 }
-                else System.out.print("  ");
+                else System.out.print(" ");
             }
             System.out.println();
         }
