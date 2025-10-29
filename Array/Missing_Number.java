6eee5f5f3d0d805c73cc8b68570ccd3acaa125ea
VINOTHPADMANABAN2809 <vinothkumarpadmanabhan@example.com>
2025-10-29 15:09:02 +0530

    Update

--- Array/Missing_Number.java
+++ Array/Missing_Number.java
@@ -5,7 +5,7 @@ public class Missing_Number {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int arr[]=new int[n];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             arr[i]=sc.nextInt();
         }
         int total=n*(n+1)/2;
