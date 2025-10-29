3d945dd25cf41e9aa4b64b2871bacbdad0e22fb0
VINOTHPADMANABAN2809 <vinothkumarpadmanabhan@example.com>
2025-10-29 14:41:58 +0530

    Missing Number

---
+++ Array/Missing_Number.java
@@ -0,0 +1,19 @@
+package Array;
+import java.util.*;
+public class Missing_Number {
+    public static void main(String[] vk){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int total=n*(n+1)/2;
+        int sum=0;
+        for(int i=0;i<n-1;i++){
+            sum+=arr[i];
+        }
+        System.out.println(total-sum);
+       sc.close();
+    }
+}
